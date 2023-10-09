1dc85f6667f87082d4419a305f5bf9915c7303d0
SuperFire-92 <nickdyk21@gmail.com>
2023-10-09 15:24:01 -0400

    ObjectSpawner
    Added an Object Spawner script that does not work as intended currently, needs a lot of work.

--- 2D Endless Runner/Assets/Scripts/BasicFalling.cs
+++ 2D Endless Runner/Assets/Scripts/BasicFalling.cs
@@ -2,23 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Wall : MonoBehaviour
+public class BasicFalling : MonoBehaviour
 {
-    public float wallSpeed;
+    public float speed;
     public bool dead;
-    // Update is called once per frame
     private void Start()
     {
         dead = false;
         if (transform.position.x > 0)
         {
             transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
-            GetComponentInChildren<ObjectSpawner>().speed = wallSpeed;
         }
     }
     void Update()
     {
-        transform.position = new Vector3(transform.position.x, transform.position.y - wallSpeed * Time.deltaTime, transform.position.z);
+        transform.position = new Vector3(transform.position.x, transform.position.y - speed * Time.deltaTime, transform.position.z);
         if (transform.position.y < -20)
         {
             dead = true;
@@ -27,12 +25,11 @@ public class Wall : MonoBehaviour
 
     public void DestroyObject()
     {
-        GetComponentInChildren<ObjectSpawner>().DestroyObject();
         Destroy(this.gameObject);
     }
 
     public void pauseObject()
     {
-        wallSpeed = 0;
+        speed = 0;
     }
 }
